5fc1dc62502292fad5f28aac2b3fb4935704b252
GaborZeller <gabriel.d.celery@gmail.com>
2025-05-12 23:15:41 +0100

    feat: replaced hardcoded resource reference values with terraform imports and added admin consent to default directory

--- terraform/modules/azure/main.tf
+++ terraform/modules/azure/main.tf
@@ -2,6 +2,8 @@ data "azurerm_subscription" "homelab_subscription" {
   subscription_id = var.azure_homelab_subscription_id
 }
 
+data "azuread_application_published_app_ids" "well_known" {}
+
 resource "azurerm_consumption_budget_subscription" "homelab_montly_budget" {
   name            = "homelab_montly_budget"
   subscription_id = data.azurerm_subscription.homelab_subscription.id
@@ -16,6 +18,11 @@ resource "azurerm_consumption_budget_subscription" "homelab_montly_budget" {
     operator       = "GreaterThanOrEqualTo"
     contact_emails = [var.azure_admin_email]
   }
+}
+
+resource "azuread_service_principal" "msgraph" {
+  client_id    = data.azuread_application_published_app_ids.well_known.result.MicrosoftGraph
+  use_existing = true
 }
 
 resource "azuread_application" "cloudflare_sso" {
@@ -34,34 +41,34 @@ resource "azuread_application" "cloudflare_sso" {
   }
 
   required_resource_access {
-    resource_app_id = "00000003-0000-0000-c000-000000000000" # Microsoft Graph API ID
+    resource_app_id = data.azuread_application_published_app_ids.well_known.result.MicrosoftGraph
     # Delegated permissions
     resource_access {
-      id   = "e1fe6dd8-ba31-4d61-89e7-88639da4683d" # User.Read
+      id   = azuread_service_principal.msgraph.oauth2_permission_scope_ids["User.Read"]
       type = "Scope"
     }
     resource_access {
-      id   = "37f7f235-527c-4136-accd-4a02d197296e" # openid
+      id   = azuread_service_principal.msgraph.oauth2_permission_scope_ids["openid"]
       type = "Scope"
     }
     resource_access {
-      id   = "64a6cdd6-aab1-4aaf-94b8-3cc8405e90d0" # email
+      id   = azuread_service_principal.msgraph.oauth2_permission_scope_ids["email"]
       type = "Scope"
     }
     resource_access {
-      id   = "7427e0e9-2fba-42fe-b0c0-848c9e6a8182" # offline_access
+      id   = azuread_service_principal.msgraph.oauth2_permission_scope_ids["offline_access"]
       type = "Scope"
     }
     resource_access {
-      id   = "14dad69e-099b-42c9-810b-d002981feec1" # profile
+      id   = azuread_service_principal.msgraph.oauth2_permission_scope_ids["profile"]
       type = "Scope"
     }
     resource_access {
-      id   = "bc024368-1153-4739-b217-4326f2e966d0" # GroupMember.Read.All
+      id   = azuread_service_principal.msgraph.oauth2_permission_scope_ids["GroupMember.Read.All"]
       type = "Scope"
     }
     resource_access {
-      id   = "06da0dbc-49e2-44d2-8312-53f166ab848a" # Directory.Read.All
+      id   = azuread_service_principal.msgraph.oauth2_permission_scope_ids["Directory.Read.All"]
       type = "Scope"
     }
   }
@@ -71,14 +78,14 @@ resource "azuread_service_principal" "cloudflare_sso" {
   client_id = azuread_application.cloudflare_sso.client_id
 }
 
-# resource "azuread_service_principal_delegated_permission_grant" "cloudflare_sso" {
-#   service_principal_object_id          = azuread_service_principal.cloudflare_sso.object_id
-#   resource_service_principal_object_id = azuread_application.cloudflare_sso.object_id
-#   claim_values = [
-#     "GroupMember.Read.All",
-#     "Directory.Read.All"
-#   ]
-# }
+resource "azuread_service_principal_delegated_permission_grant" "cloudflare_sso" {
+  service_principal_object_id          = azuread_service_principal.cloudflare_sso.object_id
+  resource_service_principal_object_id = azuread_service_principal.msgraph.object_id
+  claim_values = [
+    "GroupMember.Read.All",
+    "Directory.Read.All"
+  ]
+}
 
 resource "azuread_application_password" "cloudflare_sso" {
   application_id = azuread_application.cloudflare_sso.id
