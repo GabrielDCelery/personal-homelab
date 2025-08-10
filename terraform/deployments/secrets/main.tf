345068ab1be1d6dab6d07e85dab797ce975b971a
GaborZeller <gabriel.d.celery@gmail.com>
2025-08-10 20:13:47 +0100

    fix: added key vault access policy to secrets terraform deployment

--- terraform/deployments/secrets/main.tf
+++ terraform/deployments/secrets/main.tf
@@ -1,3 +1,5 @@
+data "azurerm_client_config" "current" {}
+
 resource "azurerm_resource_group" "secrets" {
   name       = "rg-gazelab-glob-secrets"
   location   = var.azure_region
@@ -8,11 +10,32 @@ resource "azurerm_key_vault" "secrets" {
   name                          = "kv-gazelab-glob-secrets"
   resource_group_name           = azurerm_resource_group.secrets.name
   location                      = azurerm_resource_group.secrets.location
-  tenant_id                     = var.azure_homelab_tenant_id
+  tenant_id                     = data.azurerm_client_config.current.tenant_id
   sku_name                      = "standard"
   soft_delete_retention_days    = 90
   purge_protection_enabled      = false
   public_network_access_enabled = true
+}
+
+resource "azurerm_key_vault_access_policy" "secrets" {
+  key_vault_id = azurerm_key_vault.secrets.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = data.azurerm_client_config.current.object_id
+
+  key_permissions = [
+    "Get",
+    "List"
+  ]
+
+  secret_permissions = [
+    "Get",
+    "List"
+  ]
+
+  storage_permissions = [
+    "Get",
+    "List"
+  ]
 }
 
 
