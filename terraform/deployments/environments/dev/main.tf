abbeddb87764f1b2f855701739d415cd0886a4ef
GaborZeller <gabriel.d.celery@gmail.com>
2025-10-28 23:42:07 +0000

    refactor: moved dev deployment outputs to dedicated file

--- terraform/deployments/environments/dev/main.tf
+++ terraform/deployments/environments/dev/main.tf
@@ -120,25 +120,3 @@ resource "digitalocean_droplet" "homelab" {
   ssh_keys = [digitalocean_ssh_key.homelab_digitalocean_server_public_key.fingerprint]
 }
 
-output "homelab_do_ipv4" {
-  value = digitalocean_droplet.homelab.ipv4_address
-}
-
-output "homelab_do_server_private_key" {
-  value = {
-    vault_name  = azurerm_key_vault.homelab.name
-    secret_name = azurerm_key_vault_secret.homelab_digitalocean_server_private_key.name
-  }
-}
-
-output "ansible_inventory" {
-  value = yamlencode({
-    homelab = {
-      hosts = {
-        do_dev = {
-          ansible_host = digitalocean_droplet.homelab.ipv4_address
-        }
-      }
-    }
-  })
-}
