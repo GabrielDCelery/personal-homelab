c9321a84b592f0067499dc173dd5307c9a34f5dc
GaborZeller <gabriel.d.celery@gmail.com>
2025-12-26 12:37:14 +0000

    chore: fixed rightmost operator provider versioning in core

--- deployments/core/provider.tf
+++ deployments/core/provider.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4"
+      version = "~> 4.0"
     }
     # azuread = {
     #   source  = "hashicorp/azuread"
@@ -11,7 +11,7 @@ terraform {
     # }
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "~> 5"
+      version = "~> 5.0"
     }
   }
   backend "azurerm" {
