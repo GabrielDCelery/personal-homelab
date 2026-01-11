26047b60e5cfe7671a5f6ae482bcfa6fc9015b94
GaborZeller <gabriel.d.celery@gmail.com>
2026-01-11 19:14:52 +0000

    chore: removed archive section

--- archive/terraform/deployments/environments/prod/main.tf
+++
@@ -1,9 +0,0 @@
-module "blog" {
-  source                         = "../../../../blog/infrastructure"
-  domain_name                    = var.domain_name
-  aws_account_id                 = var.aws_account_id
-  cloudflare_zone_id             = var.cloudflare_zone_id
-  cloudflare_domain_cname_name   = var.cloudflare_domain_cname_name
-  cloudflare_ipv4_address_ranges = compact(split("\n", file("${path.module}/files/cloudflare-ipv4-ranges.txt")))
-}
-
--- archive/terraform/modules/azure/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "azure_admin_email" {
-  description = "Admin email for my Azure account"
-  type        = string
-}
-
-variable "azure_homelab_subscription_id" {
-  description = "The ID of the homelab subscription"
-  type        = string
-}
