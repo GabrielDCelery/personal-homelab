5902f680c8ff119e0728edf1767259d47a5b2dd1
GaborZeller <gabriel.d.celery@gmail.com>
2026-08-23 14:24:22 +0100

    feat: enhanced jellyfin on homepage

---
+++ scripts/k8s-deploy-homepage-secret.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+kubectl create secret generic homepage-credentials \
+  --namespace default \
+  --from-literal=jellyfinApiKey="$jellyfin_api_key" \
+  --dry-run=client -o yaml | kubectl apply -f -
